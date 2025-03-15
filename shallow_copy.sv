ad0e1794e5b5923dfab0cc87d896610517439135
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-03-15 13:16:52 +0530

    Create shallow_copy.sv

---
+++ shallow_copy.sv
@@ -0,0 +1,34 @@
+//shallow copy and class assignments are same
+module scopy_tb;
+  class shallow_copy;
+    static int a; 
+    int b;        
+
+    function new();
+      a = 1; 
+    endfunction
+  endclass
+
+  shallow_copy shcopy; 
+  initial begin
+    shcopy = new(); 
+    
+
+    
+
+    shcopy.b = 25;
+
+    $display("before modification a = %0d", shcopy.a); 
+    $display("before modification b = %0d", shcopy.b);
+    shcopy.b = shcopy.a;
+    
+    $display("after  modification a = %0d", shcopy.a); 
+    $display("after  modification b = %0d", shcopy.b);
+  end
+endmodule
+
+
+# before modification a = 1
+# before modification b = 25
+# after  modification a = 1
+# after  modification b = 1
