73cb2bd91160f7cdf7e6a7eb81123b7e0338f30a
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-03-24 18:42:08 +0530

    Update constraint_inside.sv

--- Day 7/constraint_inside.sv
+++ Day 7/constraint_inside.sv
@@ -19,3 +19,8 @@ module tb;
     endmodule
     
   
+// # rand values of address=196,start_addr=152,end_addr=215
+// # rand values of address=56,start_addr= 31,end_addr=64
+// # rand values of address=93,start_addr= 89,end_addr=231
+// # rand values of address=236,start_addr=  0,end_addr=242
+// # rand values of address=135,start_addr=112,end_addr=228
