6baa25e5842b967ed1a7b71f1683115729b89e1d
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-04-03 18:20:59 +0530

    Create 2disable_fork.sv

---
+++ Day 9/2disable_fork.sv
@@ -0,0 +1,36 @@
+module fork_join_example;
+
+  initial begin
+    fork
+      repeat(2)begin
+        $display("Process A started at time = %0t", $time);
+        $display("Process A completed at time = %0t", $time);
+    
+        $display("Process B started at time = %0t", $time);
+        #30;
+        $display("Process B completed at time = %0t", $time);
+      
+        $display("Process C started at time = %0t", $time);
+        $display("Process C completed at time = %0t", $time);
+      end
+    join_any
+    disable fork;
+    #40;
+      $display("fork-join completed at time = %0t", $time);
+     
+  end
+endmodule
+
+Process A started at time = 0
+Process A completed at time = 0
+Process B started at time = 0
+Process B completed at time = 30
+Process C started at time = 30
+Process C completed at time = 30
+Process A started at time = 30
+Process A completed at time = 30
+Process B started at time = 30
+Process B completed at time = 60
+Process C started at time = 60
+Process C completed at time = 60
+fork-join completed at time = 100
