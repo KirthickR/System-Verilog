a1f340835c31f6630b6a928ece38e860823405f7
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-05-17 14:54:04 +0530

    Create transaction.sv

---
+++ DAY 10/COVERAGE/HALFADDER VERIFICATION/transaction.sv
@@ -0,0 +1,23 @@
+class transaction;
+  randc bit a;
+    randc bit b;
+  bit sum,carry;
+  
+
+
+// function void post_randomize();
+//   a=ab[0];
+//   b=ab[1];
+// endfunction 
+
+function void display(string name);
+  $display("signals=%s",name);
+  
+  $display("a=%b,b=%b,sum=%b,carry=%b", a,b,sum,carry);
+endfunction
+
+function void display_l();
+  $display("******************************");
+endfunction
+  
+endclass
