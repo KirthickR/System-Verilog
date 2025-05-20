6f313abd26fcc4fd7f107c585b606a7ee55c7e44
Kirthickroshan R <138604727+KirthickR@users.noreply.github.com>
2025-05-20 10:33:05 +0530

    Create testbench.sv

---
+++ DAY 10/COVERAGE/D FLIPFLOP/testbench.sv
@@ -0,0 +1,21 @@
+`include "interface.sv"
+`include "test.sv"
+module testbench;
+  intf intff();
+    test tst(intff);
+    
+    initial intff.clk = 0;
+    always #2 intff.clk = ~intff.clk;
+    
+    dff dut(.q(intff.q),
+            .qbar(intff.qbar),
+            .clk(intff.clk),
+            .rst(intff.rst),
+            .d(intff.d));
+  
+  initial begin
+    #300;
+  $finish;
+  end
+    
+    endmodule
